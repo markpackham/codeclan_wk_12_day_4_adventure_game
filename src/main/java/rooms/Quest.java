40a3d0129cb568a23cf2d976ae45a941d5e829a6
mark <markpackham1@gmail.com>
2020-06-25 14:10:38 +0100

    Test can add room

---
+++ src/main/java/rooms/Quest.java
@@ -0,0 +1,25 @@
+package rooms;
+
+import java.util.ArrayList;
+
+public class Quest {
+
+    private ArrayList<Room> rooms;
+
+    public Quest() {
+        this.rooms = new ArrayList<Room>();
+    }
+
+    public ArrayList<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(ArrayList<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+
+    public void addRoom(Room room) {
+        this.rooms.add(room);
+    }
+}
---
+++ src/test/java/rooms/QuestTest.java
@@ -0,0 +1,26 @@
+package rooms;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuestTest {
+
+    Quest quest1;
+    Room chamber1;
+
+
+    @Before
+    public void setUp() throws Exception {
+        chamber1 = new TortureChamber(0);
+        quest1 = new Quest();
+    }
+
+    @Test
+    public void canAddRoom(){
+        quest1.addRoom(chamber1);
+        assertEquals(1,quest1.getRooms().size());
+    }
+
+}
